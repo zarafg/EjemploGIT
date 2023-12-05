3ae4459fca6ba747d0d571780efc406064051b27
zarafg <151996001+zarafg@users.noreply.github.com>
2023-12-05 16:59:18 +0100

    Update domingo2.java

--- PruebaGit/src/main/java/modelo/MiClase.java
+++ PruebaGit/src/main/java/modelo/MiClase.java
@@ -6,7 +6,7 @@ public class MiClase implements Miinterfaz{
 	
 	public int metododevuelveentero() {
 		//Auto-generated method stub
-		
+		//No me dio tiempo de editar este metodo
 		
 		return 0;
 	}
