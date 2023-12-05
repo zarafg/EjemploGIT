35cf5db62daf38a51cf03d2b42609a90c70b0ea0
zaraf <zaraf@192.168.1.130>
2023-12-05 15:25:59 +0100

    Primera semana de trabajo

--- PruebaGit/src/main/java/principal/Principal.java
+++ PruebaGit/src/main/java/principal/Principal.java
@@ -4,7 +4,12 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		System.out.println("Comienozo de mi programa dificilísimo");
+		System.out.println("Sigo con mi programa dificilísimo");
+		System.out.println("Añado funcionalisades a mi programa dificilísimo");
+		System.out.println("Estoy muy contento con mi programa dificilísimo");
+		
 	}
 
 }
