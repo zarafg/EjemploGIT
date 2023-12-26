632d9620eab0ca2b4483acb365c680d864c304e0
zarafg <151996001+zarafg@users.noreply.github.com>
2023-12-26 17:19:39 +0100

    Update MiClase.java

--- PruebaGit/src/main/java/modelo/MiClase.java
+++ PruebaGit/src/main/java/modelo/MiClase.java
@@ -7,7 +7,7 @@ public class MiClase implements Miinterfaz{
 	public int metododevuelveentero() {
 		//Auto-generated method stub
 		//No me dio tiempo de editar este metodo
-		
+		// No me dio tiempo
 		return 0;
 	}
 	
